e9c68da4759223de56643c114d87ae49cbde99a0
Nguyễn Lê Hoàng Trung <generaltrung@gmail.com>
2018-08-01 07:37:31 +0700

    Check if download interrupted, then resume

--- fshare.py
+++ fshare.py
@@ -61,7 +61,7 @@ class Fshare:
         for link in link_list:
             l = self.get_link("www.fshare.vn/file/" + link['linkcode'])
             print(link['name'])
-            cmd = ['wget', '--restrict-file-names=nocontrol', '-nc', l]
+            cmd = ['wget', '--restrict-file-names=nocontrol', '--continue', l]
             env = os.environ.copy()
             env['LD_LIBRARY_PATH'] = ''
             p = subprocess.Popen(cmd, shell=False, preexec_fn=set_pdeathsig(signal.SIGTERM), env=env)

7f5ae0df4b3602b73a1e33acaf190fc9e614b480
generaltrung <generaltrung@gmail.com>
2019-11-13 05:39:50 +0700

    fetch full folder tree

--- fetch_folder_des.py
+++ fetch_folder_des.py
@@ -12,16 +12,27 @@ with open(os.path.join(os.path.dirname(__file__), 'acc_info.json'), 'r') as fp:
 fshare = Fshare(email=acc_info['email'], password=acc_info['pass'])
 
 
-def fetch_folder(folder_link, file_name, path=None):
+def fetch_folder_tree(folder_link):
     list_link = fshare.get_folder_info(folder_link)
     result = []
     s = 0
     for item in list_link:
-        name = item['name']
-        link = "https://www.fshare.vn/file/" + item['linkcode']
-        size = item['size']
-        s = s + size / 1024 / 1024
-        result.append({'name': name, 'link': link, 'size': size})
+        if item['type'] == 1:
+            name = item['name']
+            link = "https://www.fshare.vn/file/" + item['linkcode']
+            size = item['size']
+            path = item['path']
+            s = s + size / 1024 / 1024
+            result.append({'name': name, 'link': link, 'size': size, 'path': path})
+        if item['type'] == 0:
+            (child_list, child_size) = fetch_folder_tree("https://www.fshare.vn/folder/" + item['linkcode'])
+            result.extend(child_list)
+            s = s + child_size
+
+    return result, s
+
+def fetch_folder(folder_link, file_name, path=None):
+    (result, s) = fetch_folder_tree(folder_link)
 
     name_path = file_name
     if path is not None:
